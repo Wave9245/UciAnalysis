4b78a6cfc0a4726720585729bf3ac3f0d4a1aa47
Wave9245 <Microwave9245@gmail.com>
2024-03-28 16:06:26 +0700

    Update Statistic.py

--- pages/Statistic.py
+++ pages/Statistic.py
@@ -4,9 +4,11 @@ import pandas as pd
 st.image('Raisin.jpg')
 cols1,cols2,cols3=st.columns(3)
 with cols1:
+    st.write("")
 with cols2: 
     st.image('Raisin.jpg')
 with cols3: 
+    st.write("")
 dt=pd.read_csv('./data/Raisin_Dataset.csv')
 html_1 = """
 <div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
