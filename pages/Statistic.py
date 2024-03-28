6e4d732267630abb5c3fca32fdb646007485fce8
Wave9245 <Microwave9245@gmail.com>
2024-03-28 15:54:20 +0700

    Update Statistic.py

--- pages/Statistic.py
+++ pages/Statistic.py
@@ -1,15 +1,21 @@
 import streamlit as st
 import pandas as pd
 dt=pd.read_csv('./data/Raisin_Dataset.csv')
-st.subheader("ข้อมูลลูกเกต")
-st.write(dt.head(7))
-
 html_1 = """
 <div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>st.subheader("สถิติข้อมูลลูกเกต")</h5></center>
+<center><h5>"ข้อมูลลูกเกต"</h5></center>
 </div>
 """
 st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
+st.write(dt.head(7))
+
+html_2 = """
+<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>"สถิติข้อมูลลูกเกต"</h5></center>
+</div>
+"""
+st.markdown(html_2, unsafe_allow_html=True)
 st.markdown("")
 st.write("ผลรวม")
 cl1,cl2,cl3,cl4,cl5,cl6,cl7=st.columns(7)
