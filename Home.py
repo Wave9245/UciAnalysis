57cbba0a62cdb09075c2f6ae7c92b3ff06be2f14
Wave9245 <Microwave9245@gmail.com>
2024-03-28 15:50:50 +0700

    .

--- Home.py
+++ Home.py
@@ -1,11 +1,3 @@
-html_1 = """
-<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>สถิติข้อมูลดอกไม้</h5></center>
-</div>
-"""
-st.markdown(html_1, unsafe_allow_html=True)
-st.markdown("")
-
 import streamlit as st
 from streamlit_lottie import st_lottie
 from streamlit_lottie import st_lottie_spinner
