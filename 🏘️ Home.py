32928f40fbbf5e00f62e20d1ad1b5294601f9871
Wave9245 <Microwave9245@gmail.com>
2024-03-29 10:43:14 +0700

    .

--- 🏘️ Home.py
+++ 🏘️ Home.py
@@ -12,7 +12,7 @@ lottie_url_hello = "https://lottie.host/377a24ee-4e19-48c9-8a2f-fcada962c70a/oax
 lottie_hello = load_lottieurl(lottie_url_hello)
 st_lottie(lottie_hello)
 
-st.page_link("Home.py", label="หน้าแรก", icon="🏠")
-st.page_link("pages/Statistic.py", label="การนำเสนอข้อมูลถั่วแห้งด้วยสถิติ", icon="1️⃣")
+st.page_link("🏘️ Home.py", label="หน้าแรก", icon="🏠")
+st.page_link("pages/🧮 1Statistic.py", label="การนำเสนอข้อมูลถั่วแห้งด้วยสถิติ", icon="1️⃣")
 st.page_link("pages/Chart.py", label="การนำเสนอข้อมูลถั่วแห้งด้วยจินตทัศน์ข้อมูล", icon="2️⃣", disabled=False)
 st.page_link("http://www.google.com", label="Google", icon="🌎")
