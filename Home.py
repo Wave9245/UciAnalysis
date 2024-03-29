270752ebd5e3397358eefdda0b5faa1bf3706475
Wave9245 <Microwave9245@gmail.com>
2024-03-29 10:54:56 +0700

    .

--- Home.py
+++ Home.py
@@ -12,7 +12,7 @@ lottie_url_hello = "https://lottie.host/377a24ee-4e19-48c9-8a2f-fcada962c70a/oax
 lottie_hello = load_lottieurl(lottie_url_hello)
 st_lottie(lottie_hello)
 
-st.page_link("🏘️ Home.py", label="หน้าแรก", icon="🏠")
+st.page_link("Home.py", label="หน้าแรก", icon="🏠")
 st.page_link("pages/1🧮 Statistic.py", label="การนำเสนอข้อมูลลูกเกตด้วยสถิติ", icon="1️⃣")
 st.page_link("pages/2📊 Chart.py", label="การนำเสนอข้อมูลลูกเกตด้วยจินตทัศน์ข้อมูล", icon="2️⃣", disabled=False)
 st.page_link("pages/3🎡 Classsification.py", label="การจำแนกข้อมูลด้วย KNN", icon="2️⃣", disabled=False)
