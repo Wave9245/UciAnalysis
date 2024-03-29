2ab622abce7220da41f01a554632486ff8db1406
Wave9245 <Microwave9245@gmail.com>
2024-03-29 14:38:41 +0700

    Update 6📈 RegressionPrediction.py

--- pages/6📈 RegressionPrediction.py
+++ pages/6📈 RegressionPrediction.py
@@ -43,13 +43,7 @@ html_1 = """
 st.markdown(html_1, unsafe_allow_html=True)
 st.markdown("") 
 
-html_2 = """
-<div sborder-radius:15px 15px 15px 15px;">
-<center>st.image("./pic/predict.jpg")</center>
-</div>
-"""
-st.markdown(html_2, unsafe_allow_html=True)
-st.markdown("") 
+st.image("./pic/predict.jpg")
 if st.button("พยากรณ์ข้อมูล"):
     x_input=[[x1,x2]]
     y_predict=modelRegress.predict(pf.fit_transform(x_input))
